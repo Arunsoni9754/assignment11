ee964fb2f8aa83849a1f8570058094d6da24865e
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 21:34:40 +0530

    Create Q8.c

---
+++ Q8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void pascal(int);
+int main()
+{
+    int n;
+    printf("Enter the number of rows ");
+    scanf("%d",&n);
+    pascal(n);
+    return 0;
+}
+void pascal(int a)
+{
+    int i,j,c;
+}
